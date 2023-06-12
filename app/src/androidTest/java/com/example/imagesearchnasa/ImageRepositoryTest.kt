86a655a225adb6f725ed19d2f3c58f2d29819f3d
Manu Verma <vermamanu0420@gmail.com>
2023-06-12 10:51:23 -0700

    repository unit test added, used mockito

---
+++ app/src/androidTest/java/com/example/imagesearchnasa/ImageRepositoryTest.kt
@@ -0,0 +1,42 @@
+package com.example.imagesearchnasa
+
+import com.example.imagesearchnasa.model.Collection
+import com.example.imagesearchnasa.model.ImageDetailModel
+import com.example.imagesearchnasa.model.Metadata
+import com.example.imagesearchnasa.repository.ImageRepository
+import com.example.imagesearchnasa.retrofit.ImageApi
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+
+import org.mockito.junit.MockitoJUnitRunner
+import retrofit2.Response
+
+
+@RunWith(MockitoJUnitRunner::class)
+class ImageRepositoryTest {
+    @Mock
+    private lateinit var imageApi: ImageApi
+    private lateinit var imageRepository: ImageRepository
+
+    @Before
+    fun initMocks() {
+        imageRepository = ImageRepository(imageApi)
+    }
+
+    @Test
+    fun test_Success_APi_Response()= runBlocking{
+        val mockData = ImageDetailModel(Collection("link", emptyList(), emptyList(),Metadata(1),"1.0"))
+        val mockResponse = Response.success(mockData)
+        `when`(imageApi.getImages("moon","image",1)).thenReturn(mockResponse)
+
+
+        assertEquals(mockData.collection.items, imageRepository.getImages("moon","image",1))
+
+    }
+}
+
